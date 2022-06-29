d22b31e5657f3739f71bba71843d0ada72708057
tpercy <tonnypercy@gmail.com>
2022-06-28 21:53:50 -0500

    creando servidor con express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-console.log("Funcionando");
+const express = require('express');
+const config = require('./config');
+
+const app = express();
+
+app.listen(config.port, () => {
+    console.log("API Listening on port: ", config.port);
+});
